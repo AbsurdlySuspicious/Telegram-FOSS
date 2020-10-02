7f7f502852533c03d1d92951c228c8a85e852c7c
mfgm2 <repom2@airmail.cc>
2020-10-02 12:52:11 +0300

    [F] Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export NINJA_PATH=$(which ninja)
+export NDK=$(rg 'ndk.dir=(.+)$' -or '$1' local.properties)
+
+git submodule deinit --all -f
+git submodule update --recursive --init -f
+
+pushd TMessagesProj/jni
+./build_ffmpeg_clang.sh
+./patch_ffmpeg.sh
+./patch_boringssl.sh
+./build_boringssl.sh
+popd
+
+./gradlew assembleAfatRelease && ./gradlew --stop
+
