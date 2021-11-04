569d0af02f5b695f4e199832020ce444320b52bd
mfgm2 <repom2@airmail.cc>
2021-11-04 23:51:58 +0300

    [F] remove ads and screenshot protection

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -4364,7 +4364,7 @@ public class MessagesController extends BaseController implements NotificationCe
     }
 
     public boolean isChatNoForwards(TLRPC.Chat chat) {
-        if (chat == null) {
+        /*if (chat == null) {
             return false;
         }
         if (chat.migrated_to != null) {
@@ -4373,7 +4373,8 @@ public class MessagesController extends BaseController implements NotificationCe
                 return migratedTo.noforwards;
             }
         }
-        return chat.noforwards;
+        return chat.noforwards;*/
+        return false;
     }
 
     public boolean isChatNoForwards(long chatId) {
@@ -16940,12 +16941,12 @@ public class MessagesController extends BaseController implements NotificationCe
                     posts_between = null;
                 } else {
                     if (res instanceof TLRPC.TL_messages_sponsoredMessages && (res.flags & 0x1) > 0) {
-                        posts_between = res.posts_between;
+                        posts_between = null;
                     } else {
                         posts_between = null;
                     }
                     result = new ArrayList<>();
-                    AndroidUtilities.runOnUIThread(() -> {
+                    /*AndroidUtilities.runOnUIThread(() -> {
                         putUsers(res.users, false);
                         putChats(res.chats, false);
                     });
@@ -16987,7 +16988,7 @@ public class MessagesController extends BaseController implements NotificationCe
                         messageObject.sponsoredAdditionalInfo = sponsoredMessage.additional_info;
                         messageObject.sponsoredWebPage = sponsoredMessage.webpage;
                         result.add(messageObject);
-                    }
+                    }*/
                 }
             } else {
                 result = null;
