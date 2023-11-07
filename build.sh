a22beb5d84dff5d665f9c4c3e14fafc0652bbfb2
mfgm2 <repom2@airmail.cc>
2023-11-07 16:39:38 +0300

    [F] better build script

--- build.sh
+++ build.sh
@@ -4,16 +4,29 @@ set -e
 export NINJA_PATH=$(which ninja)
 export NDK=$(rg 'ndk.dir=(.+)$' -or '$1' local.properties)
 
-git submodule deinit --all -f
-git submodule update --recursive --init -f
+submodule_init() {
+  git submodule deinit --all -f
+  git submodule update --recursive --init -f
+}
 
-pushd TMessagesProj/jni
-./build_libvpx_clang.sh
-./build_ffmpeg_clang.sh
-./patch_ffmpeg.sh
-./patch_boringssl.sh
-./build_boringssl.sh
-popd
+jni_build() {
+  pushd TMessagesProj/jni
+  ./build_libvpx_clang.sh
+  ./build_ffmpeg_clang.sh
+  ./patch_ffmpeg.sh
+  ./patch_boringssl.sh
+  ./build_boringssl.sh
+  popd
+}
 
-./gradlew assembleAfatRelease && ./gradlew --stop
+gradle_build() {
+  ./gradlew assembleAfatRelease && ./gradlew --stop
+}
+
+if [[ "$GRADLE_ONLY" != 1 ]]; then
+  submodule_init
+  jni_build
+fi
+gradle_build
+
 
